9105c741400c4465a24d1de3082ca18252110cfe
Jusung Lee <e.jusunglee@gmail.com>
2017-10-21 18:41:43 -0400

    hella

--- app.py
+++ app.py
@@ -4,16 +4,6 @@ from flask import request
 from flask import jsonify
 from play import get_d, is_congress
 
-consumer_secret = 'JhrlYNlZKu8oOpaZP16xh8tmaxyPOJqz6ZFGa5QM3TUD2dVWtI'
-consumer_key = 'qXTf2YnuBiCMgj6i8jniAV8WW'
-access_token = '395269803-yGLNDGa5VOCywOF9gGRNebtEGYmS8zTR42zKDJww'
-access_token_secret = 'TEQHGo8rTEYonFNuwQYgY6TbgApg3CDFS0HT8y1d4YHEU'
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
-api = tweepy.API(auth)
-app = Flask(__name__)
-app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
-
 
 @app.route('/')
 def dashboard():
