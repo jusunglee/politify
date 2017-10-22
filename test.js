e6aba754feb0114cd276329a8cd395366d44d6a8
Jun Jang <junjang@college.harvard.edu>
2017-10-22 02:55:47 -0400

    chrome ready for integration w/ css

--- test.js
+++ test.js
@@ -19,7 +19,7 @@ $.post(
     JSON.stringify({
       'api_key': "5459fe6fddd22c1b76d3dc6204928e50",
       'data': getTextParagraph(),
-      'threshold': 0.01
+      'threshold': 0.1
     })
   ).then(function(res) { postProcess(res) });
 
@@ -32,49 +32,159 @@ function postProcess(data){
     {
         if(jsondata["results"][i]["text"].split(" ").length == 2){
             names[i] = jsondata["results"][i]["text"];
-            
         }
         // send list to API for processing
 
     }
     var data = {"list": names};
     console.log(data);
-    $.post(
-        "https://politify.herokuapp.com/getlist",
-        data
-      ).then(function(res) { underlineText(res) });
 
+    if (names.length > 0)
+    {
+        $.post(
+            "https://politify.herokuapp.com/getlist",
+            data
+        ).then(function(res) { underlineText(res) });
+    }
 }
 
 function underlineText(name_list)
 {
-    console.log(name_list);
     var paragraph_array = document.getElementsByTagName("p");
     
     var array_len = paragraph_array.length;
+  
+    // For each paragraph
     for (var i = 0; i < array_len; i++)
     {
+        if(paragraph_array[i].innerHTML.includes("<em>"))
+        {
+            continue;
+        }
+        else if(paragraph_array[i].innerHTML.includes("&nbsp;"))
+        {
+            continue;
+        }
+        else if(paragraph_array[i].innerHTML.includes("<strong>"))
+        {
+            continue;
+        }
+        else if(paragraph_array[i].className.includes("interstitial"))
+        {
+            continue;
+        }
         var split_paragraph = paragraph_array[i].innerHTML.split(" ");
+        console.log(split_paragraph);
         var split_length = split_paragraph.length;
         
         var isName = {};
-        for (name in name_list)
+       
+        var name_len = name_list["results"].length;
+        //here, strip words before sending in
+        for (var j = 0; j < name_len; j++)
         {
-            isName[name_list[name]] = true;
+            // curr_word = name_list["results"][j];
+            // console.log(curr_word);
+            isName[name_list["results"][j]] = true;
+            
         }
+        
         
         var index_array = [];
 
         for (var j = 0; j < (split_length - 1); j++)
         {
+           if (split_paragraph[j].includes("&nbsp;"))
+           {
+              split_paragraph[j] = split_paragraph[j].replace(/&nbsp;/, " ");
+            //   console.log(split_paragraph[j]);
+           }
+           else if (split_paragraph[j+1].includes("&nsbp;"))
+           {
+               split_paragraph[j+1] = split_paragraph[j+1].replace(/&nbsp;/, " ");
+            //    console.log(split_paragraph[j+1]);
+           }
+         
+
            var word_pair = split_paragraph[j] + " " + split_paragraph[j+1];
+           
+        //    console.log("word1 after pair call");
+        //    console.log(split_paragraph[j]);
            console.log(word_pair);
-           if (isName[word_pair] === true)
+            word_pair = word_pair.replace(/[^\w\s]|_/g, "");
+            console.log(word_pair);
+
+           if (isName[word_pair] == true)
            {
                 index_array.push(j);
            }
+    
         }
-        console.log(index_array);
+        var subindex = 0;
+        var new_p = document.createElement("p");
+        var sub = "";
+        var clean_p = document.createElement("p");
+        for (var k = 0; k < split_length; k++)
+        {
+            console.log(split_paragraph[k]);
+            
+            if (k == index_array[subindex])
+            {
+                // console.log("in word condition");
+                var newNode = document.createElement("b");
+                newNode.className = "special-links";
+                var word = document.createTextNode(split_paragraph[k] + " " + split_paragraph[k+1] + " ");
+                newNode.appendChild(word);
+                var subNode = document.createTextNode(sub);
+                new_p.appendChild(subNode);
+                new_p.appendChild(newNode);
+                sub = "";
+                subindex++;
+                k++;
+            }
+            else{
+                sub += (split_paragraph[k] + " ");
+            }
+            
+        }
+        var subNode = document.createTextNode(sub);
+        new_p.appendChild(subNode);
+        
+        
+        
+        if (new_p.innerHTML.includes(new_p.innerHTML.match(/&lt;a.*&gt;.*&lt;\/a&gt;/)))
+        {
+            var middle = new_p.innerHTML.match(/&lt;a.*&gt;(.*)&lt;\/a&gt;/)[1];
+            console.log(middle);
+            console.log("match");
+            // console.log(new_p.innerHTML.match(/&lt;a.*&gt;(.*)&lt;\/a&gt;/));
+            var test= new_p.innerHTML.match(/&lt;a.*&gt;.*&lt;\/a&gt;/);
+            // console.log(test);
+            var fixed_text= new_p.innerHTML.replace(new_p.innerHTML.match(/&lt;a.*&gt;.*&lt;\/a&gt;/)[0], middle);
+             console.log(fixed_text);
+            // console.log(sub);
+            var raw_text = new_p.innerHTML;
+            // var clean_text = raw_text.replace()
+
+            var fixed_node = document.createTextNode(fixed_text);
+            clean_p.appendChild(fixed_node);
+            // clean_p = document.createTextNode(fixed_text);
+            // console.log(clean_p);
+            // console.log(clean_p.innerHTML);
+            console.log("special html");
+            document.getElementsByTagName("p")[i].innerHTML = clean_p.innerHTML;
+        }
+        else{
+            console.log("regular html");
+            document.getElementsByTagName("p")[i].innerHTML = new_p.innerHTML;
+        }
+        // document.getElementsByTagName("p")[i].innerHTML = new_p.innerHTML;
+        // document.getElementsByTagName("p")[i].innerHTML = clean_p.innerHTML;
+        
+        
+
+
+        
     }
     
 }
@@ -82,22 +192,22 @@ function underlineText(name_list)
 
 
 
-function bold(name) {
-    var list = document.getElementsByTagName("p")[0].innerHTML;
-    var splits = list.split("change");
-    var before = document.createTextNode(splits[0]);
-    var after = document.createTextNode(splits[1]);
-    var word = document.createTextNode("destroy");
-    var newNode = document.createElement("b");
-    newNode.appendChild(word);
+// function bold(name) {
+//     var list = document.getElementsByTagName("p")[0].innerHTML;
+//     var splits = list.split("Elizabeth Warren");
+//     var before = document.createTextNode(splits[0]);
+//     var after = document.createTextNode(splits[1]);
+//     var word = document.createTextNode("destroy");
+//     var newNode = document.createElement("b");
+//     newNode.appendChild(word);
 
-    document.getElementsByTagName("p")[0].innerHTML = "";
-    document.getElementsByTagName("p")[0].appendChild(before);
-    document.getElementsByTagName("p")[0].appendChild(newNode);
-    document.getElementsByTagName("p")[0].appendChild(after);
+//     document.getElementsByTagName("p")[0].innerHTML = "";
+//     document.getElementsByTagName("p")[0].appendChild(before);
+//     document.getElementsByTagName("p")[0].appendChild(newNode);
+//     document.getElementsByTagName("p")[0].appendChild(after);
 
     
-}
+// }
 
 
 
@@ -133,3 +243,6 @@ function bold(name) {
 // document.getElementsByTagName("p")[0].innerHTML = before;
 // document.getElementsByTagName("p")[0].innerHTML = " ";
 // document.getElementsByTagName("p")[0].innerHTML = after;
+
+
+// <a.*>(.*)<\/a>
